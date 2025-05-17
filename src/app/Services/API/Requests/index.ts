4b4607a526900428131136f27cb6445a74029c22
Marco Montanari <sirmmo@gmail.com>
2025-05-17 09:31:56 +0200

    update

--- src/app/Services/API/Requests/index.ts
+++ src/app/Services/API/Requests/index.ts
@@ -44,7 +44,7 @@ export class RequestApi extends Http {
         .get<any>(BACKEND_API_URL + '/maps/map-screenshot', {
           params: {
             url: href + '&op=screenshot',
-            delay_seconds: '10',
+            delay_seconds: '12',
           },
           responseType: 'blob',
         })
